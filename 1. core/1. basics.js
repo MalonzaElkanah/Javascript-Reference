dc5508b1d7519c0eec0a057de3e16bf29cb9b6a1
MalonzaElkanah <elkanahmalonza@gmail.com>
2021-04-21 14:22:27 +0300

    Second Commit

---
+++ 1. core/1. basics.js
@@ -0,0 +1,21 @@
+/* 	1.Variables, 
+	2.data types, 
+	3.operators 
+*/
+
+//1. Variables
+	//declared by var
+var x;
+var y;
+var z;
+
+var x = 1; //declare variable and provide them with value(initializing)
+var y = 2;
+var z = 3;
+y = 12 //We can also change these values later
+
+	//declared by let 
+
+	//declared by  const
+
+//2. Data Types
