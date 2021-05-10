4e87a79e28c7b2f8e4726687fcd16c62aef83368
MalonzaElkanah <elkanahmalonza@gmail.com>
2021-05-10 15:08:04 +0300

    Introduction to reactJS Framework

---
+++ 14. front-end-libraries/1. react/introduction.js
@@ -0,0 +1,41 @@
+
+
+//1. introduction
+	//React operates not directly on the browser's Document Object Model (DOM) immediately, but on a virtual DOM
+	//ref work-space/day1.html
+
+//2. React JSX
+	//(JavaScript eXtension), is a React extension that allows us to write JavaScript that looks like HTML.
+class HelloWorld extends React.Component {
+	render() {
+		return (
+			<h1 className='large'>Hello World</h1>
+		);
+	}
+}
+
+
+//*3. React Components*/
+	//ref work-space/day3.html
+class App extends React.Component {
+	render() {
+		return <h1>Hello from our app</h1>
+	}
+}
+//*/render() function is expected to return a virtual DOM representation of the browser DOM element(s).*/
+
+
+
+//4. Complex Components
+	//ref work-space/day4.html
+
+
+
+
+//React State
+
+
+//React Props
+
+
+//
